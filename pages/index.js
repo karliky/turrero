8c153d736e76965220e9874f9fc6195aaa10e0f2
karliky <karliky@gmail.com>
2023-01-27 11:19:26 +0100

    Add SEO joke

--- pages/index.js
+++ pages/index.js
@@ -187,7 +187,7 @@ export default function Turrero() {
       </div>
       <div className="footer">
         El código fuente de este proyecto se encuentra en <a target="_blank" href="https://github.com/karliky/turrero">GitHub</a>.
-        Envía tus mejoras a <a target="_blank" href="http://www.twitter.com/k4rliky">@k4rliky</a>.<br></br>
+        Envía tus mejoras a <a target="_blank" title="Programador Guapo" href="http://www.twitter.com/k4rliky">@k4rliky</a>.<br></br>
         <span className="small">Creado con Next.js y ChatGPT.</span>
       </div>
     </div>
--- pages/turra/[id].js
+++ pages/turra/[id].js
@@ -149,7 +149,7 @@ const Post = ({ tweetId, summary, categories, tweets, enrichments, publishedDate
       </div>
       <div className="footer">
         El código fuente de este proyecto se encuentra en <a target="_blank" href="https://github.com/karliky/turrero">GitHub</a>.
-        Envía tus mejoras a <a target="_blank" href="http://www.twitter.com/k4rliky">@k4rliky</a>.<br></br>
+        Envía tus mejoras a <a target="_blank" title="Programador Guapo" href="http://www.twitter.com/k4rliky">@k4rliky</a>.<br></br>
         <span className="small">Creado con Next.js y ChatGPT.</span>
       </div>
       <style jsx global>
