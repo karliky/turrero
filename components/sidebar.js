82f66ec4063e35ca4599b4bbdd08625827b608cd
karliky <karliky@gmail.com>
2023-01-27 17:43:37 +0100

    Change text

--- components/sidebar.js
+++ components/sidebar.js
@@ -33,7 +33,7 @@ export default function ({ books, videos, linkedin, urls, wikipedia, summary, ca
             </div>}
         </div>
         <div className='side-block'>
-            <div>Comparte esta turra con el mundo:</div>
+            <div>Dale la turra a más gente:</div>
             <div className="sharing">
             <div className="social-media twitter"><a href={"https://twitter.com/intent/tweet?text=" + sharingText} target="_blank"><img className="icon" src="/twitter-white.svg" alt="Compartir en Twitter" />Compartir en Twitter</a></div>
             <div className="social-media linkedin"><a href={"http://www.linkedin.com/shareArticle?url=" + "https://turrero.vercel.app/turra/" + tweetId + "&title="+ encodeURI("La turra de @recuenco sobre " + categoriesAsText)} target="_blank"><img className="icon" src="/linkedin-white.svg" alt="Compartir en Linkedin" />Compartir en Linkedin</a></div>
