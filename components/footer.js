edca346bbe94bbce0e75bda4772fb3bf36dccd4e
karliky <karliky@gmail.com>
2023-02-01 16:41:48 +0100

    Add more questions

--- components/footer.js
+++ components/footer.js
@@ -2,6 +2,6 @@ export default function Footer() {
     return <div className="footer">
         El código fuente de este proyecto se encuentra en <a target="_blank" href="https://github.com/karliky/turrero">GitHub</a>.
         Envía tus mejoras a <a target="_blank" title="Programador Guapo" href="http://www.twitter.com/k4rliky">@k4rliky</a>.<br></br>
-        <span className="small">Creado con Next.js y ChatGPT.</span>
+        <span className="small">Creado con Next.js y ChatGPT. Algunos textos han sido generados por una Inteligencia Artificial y pueden no ser precisos o contener errores.</span>
     </div>;
 }
