9b2e03b390d7a7f15b5334a6ace42802d4246ef2
Ángel J. Ramos <angel@doit-intl.com>
2024-05-11 10:23:25 +0200

    fix: fixed execution issues

--- scripts/image-card-generator.js
+++ scripts/image-card-generator.js
@@ -1,12 +1,18 @@
-const Jimp = require("jimp");
-const TweetsSummary = require('../db/tweets_summary.json');
+import Jimp from "jimp";
+import TweetsSummary from '../db/tweets_summary.json' assert { type: 'json' };
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fileName = __dirname + '/../static/promo.png';
 const fileNameOutput = __dirname + '/../public/meta/';
 
 (async () => {
     const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    for (tweet of TweetsSummary) {
+    for (let tweet of TweetsSummary) {
         const img = await Jimp.read(fileName);
         await img.print(font, 350, 200, tweet.summary + '.', 600).write(fileNameOutput + tweet.id + '.png');
     }
