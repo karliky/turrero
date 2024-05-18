8dc1303c2ade875050b71a92aa963ab1bab8b94c
Ángel J. Ramos <angel@doit-intl.com>
2024-05-18 20:08:42 +0200

    feat: added a couple of tools for extract thread texts and to convert db to a json format

---
+++ scripts/db-to-json.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+// Cargamos el archivo JSON original.
+import Tweets from '../db/tweets.json' assert { type: 'json' };
+
+console.log(Tweets);
+
+// Transformamos el array de arrays a un array de objetos con las propiedades 'id' y 'tweets'.
+const transformedTweets = Tweets.map(tweetArray => ({
+  id: tweetArray[0].id,
+  tweets: tweetArray
+}));
+
+// Convertimos el nuevo array de objetos a un string JSON.
+const jsonString = JSON.stringify(transformedTweets, null, 2);
+
+// Guardamos el nuevo formato en un archivo JSON.
+fs.writeFile("../db/transformed_tweets.json", jsonString, err => {
+  if (err) {
+    console.error("Error al escribir el archivo", err);
+  } else {
+    console.log("Archivo guardado con éxito.");
+  }
+});
---
+++ scripts/extract_thread_text.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Verificar si se proporcionaron suficientes argumentos
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <archivo_json> <id>"
+    exit 1
+fi
+
+# Asignar argumentos a variables
+archivo_json=$1
+id=$2
+
+# Usar jq para encontrar el subarray y extraer los campos tweet sin comillas
+resultado=$(jq --raw-output --arg id "$id" '
+    .[]
+    | select(any(.[]; .id == $id))
+    | .[].tweet
+' "$archivo_json")
+
+# Comprobar si se encontró algún resultado
+if [ -z "$resultado" ]; then
+    echo "No se encontró ningún objeto con el ID proporcionado."
+else
+    echo "$resultado"
+fi
