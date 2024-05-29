f6ac2f71536dc17e25a1f1144f03bd84189c3817
Ángel J. Ramos <angel@doit-intl.com>
2024-05-29 23:43:58 +0200

    fix: mobile compatibility

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import Head from "next/head";
 import Footer from "../components/footer";
@@ -30,12 +30,15 @@ const GraphPage = () => {
       });
     });
 
-    const width = document.querySelector('.graph-container').clientWidth;
-    const height = document.querySelector('.graph-container').clientHeight;
+    const container = document.querySelector('.graph-container');
+    const width = container.clientWidth;
+    const height = container.clientHeight;
 
-    const svg = d3.select('.graph-container').append('svg')
-      .attr('width', width)
-      .attr('height', height);
+    const svg = d3.select(ref.current)
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .attr('preserveAspectRatio', 'xMidYMid meet');
 
     // Colores basados en categorías
     const color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -47,7 +50,6 @@ const GraphPage = () => {
       .force("center", d3.forceCenter(width / 2, height / 2));
 
     const g = svg.append("g");
-
 
     const link = g.append("g")
       .attr("stroke", "#999")
@@ -83,7 +85,7 @@ const GraphPage = () => {
     // Crear una escala para el tamaño de los nodos basado en las vistas
     const radiusScale = d3.scaleSqrt()
       .domain([0, d3.max(nodes, d => d.views)])
-      .range([5, 30]);
+      .range([3, 20]);
 
     const node = g.append("g")
       .attr("stroke", "#fff")
@@ -93,121 +95,142 @@ const GraphPage = () => {
       .join("g")
       .call(drag(simulation))
       .on("click", function() {
-      const d = d3.select(this).datum();
-      window.open(`/turra/${d.id}`, "_blank");
+        const d = d3.select(this).datum();
+        window.open(`/turra/${d.id}`, "_blank");
       });
 
     node.append("circle")
-      .attr("r", d => radiusScale(d.views)) // Usar la escala para establecer el radio
-      .attr("fill", d => color(d.categories[0])); // Usar el primer elemento de categorías para el color
+      .attr("r", d => radiusScale(d.views))
+      .attr("fill", d => color(d.categories[0]));
 
-    node.on("mouseover", function(event) {
-      // Crear un contenido HTML para el tooltip
-      const d = d3.select(this).datum();
-    
+    // Definir el comportamiento del tooltip
+    const showTooltip = (event, d) => {
+      const tooltip = d3.select("#tooltip");
+      tooltip.html(`<div>
+        <p style="font-size: 12px;">Click para abrir en nueva pestaña</p>
+        <h3 style="font-weight:bold;">${d.summary}</h3>   
+        <p><i>Views:</i> ${d.views}</p>
+        <p><i>Likes:</i> ${d.likes}</p>
+        <p><i>Replies:</i> ${d.replies}</p>
+        <p><i>Bookmarks:</i> ${d.bookmarks}</p>
+      </div>`)
+      .style("visibility", "visible");
+
+      // Calcular la posición del tooltip
+      const tooltipWidth = tooltip.node().offsetWidth;
+      const tooltipHeight = tooltip.node().offsetHeight;
+      let left = event.pageX + 10;
+      let top = event.pageY - 10;
+
+      // Ajustar la posición si el tooltip se sale de la pantalla
+      if (left + tooltipWidth > window.innerWidth) {
+        left = window.innerWidth - tooltipWidth - 10;
+      }
+      if (top + tooltipHeight > window.innerHeight) {
+        top = window.innerHeight - tooltipHeight - 10;
+      }
+      if (left < 0) left = 10;
+      if (top < 0) top = 10;
+
+      tooltip.style("left", `${left}px`)
+             .style("top", `${top}px`);
+    };
+
+    const hideTooltip = () => {
       d3.select("#tooltip")
-        .style("left", (event.pageX + 10) + "px")
-        .style("top", (event.pageY - 10) + "px")
-        .style("visibility", "visible")
-        .style("width", "250px")
-        .style("background-color", "#fff")  // Establecer el fondo a blanco o cualquier otro color deseado
-        .style("padding", "10px")  // Agregar algo de padding para mejor presentación
-        .style("border-radius", "5px")  // Bordes redondeados
-        .style("box-shadow", "0px 0px 5px rgba(0,0,0,0.5)")  // Sombra para el tooltip
-        .html(`<div>
-            <p style="font-size: 12px;">Click para abrir en nueva pestaña</p>
-            <h3 style="font-weight:bold;">${d.summary}</h3>   
-            <p><i>Views:</i> ${d.views}</p>
-            <p><i>Likes:</i> ${d.likes}</p>
-            <p><i>Replies:</i> ${d.replies}</p>
-            <p><i>Bookmarks:</i> ${d.bookmarks}</p>
-          </div>`);  
+        .style("visibility", "hidden");
+    };
+
+    // Añadir eventos de ratón y táctiles para el tooltip
+    node.on("mouseover", function(event) {
+      const d = d3.select(this).datum();
+      showTooltip(event, d);
     })
-    .on("mouseout", function(d) {
-      
-        d3.select("#tooltip")
-          .style("visibility", "hidden");
-      
-    });
-    
+    .on("mouseout", hideTooltip)
+    .on("touchstart", function(event) {
+      const d = d3.select(this).datum();
+      showTooltip(event.touches[0], d);
+      // Prevenir el comportamiento de zoom mientras se muestra el tooltip
+      event.preventDefault();
+    })
+    .on("touchend", hideTooltip);
 
     const zoom = d3.zoom()
-    .scaleExtent([1 / 2, 8])
-    .on("zoom", (event) => {
-      g.attr("transform", event.transform);
+      .scaleExtent([1 / 2, 8])
+      .on("zoom", (event) => {
+        g.attr("transform", event.transform);
+      });
+
+    svg.call(zoom);
+
+    // Adding legend
+    const legend = svg.append("g")
+      .attr("transform", `translate(${width - 250}, 20)`);
+
+    categories.forEach((category, index) => {
+      const legendRow = legend.append("g")
+        .attr("transform", `translate(0, ${index * 20})`);
+
+      legendRow.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", color(category));
+
+      legendRow.append("text")
+        .attr("x", 20)
+        .attr("y", 10)
+        .text(category);
     });
 
-  svg.call(zoom);
+    simulation.on("tick", () => {
+      link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
-  // Adding legend
-  const legend = svg.append("g")
-    .attr("transform", `translate(${width - 250}, 20)`);
+      node
+        .attr("transform", d => `translate(${d.x}, ${d.y})`);
+    });
 
-  categories.forEach((category, index) => {
-    const legendRow = legend.append("g")
-      .attr("transform", `translate(0, ${index * 20})`);
-
-    legendRow.append("rect")
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", color(category));
-
-    legendRow.append("text")
-      .attr("x", 20)
-      .attr("y", 10)
-      .text(category);
-  });
-
-   simulation.on("tick", () => {
-    link
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
-
-    node
-      .attr("transform", d => `translate(${d.x}, ${d.y})`);
-  });
-
-  return svg.node();
-}, []);
+    return svg.node();
+  }, []);
 
   const title = `Grafo de Turras`;
   const summary = `Grafo de como se relacionan los distintos hilos de turras entre si, por categorías, el tamaño de los nodos se corresponden con el número de views de cada hilo.`;
-return (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta content="text/html; charset=UTF-8" name="Content-Type" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-      <meta name="description" content={summary} key="desc" />
-      <meta property='og:url' content='https://turrero.vercel.app/about'/>
-      <meta property="og:title" content="Sobre el proyecto de El Turrero Post - Las turras de Javier G. Recuenco" />
-      <meta property="og:description" content={summary} />
-      <meta property="og:image" content="https://turrero.vercel.app/promo.png"/>
-      <meta name="twitter:card" content="summary_large_image"/>
-      <meta name="twitter:site" content="@recuenco"/>
-      <meta name="twitter:creator" content="@k4rliky"/>
-      <meta name="twitter:title" content="Sobre el proyecto de El Turrero Post - Las turras de Javier G. Recuenco"/>
-      <meta name="twitter:description" content={summary}/>
-      <meta name="twitter:image" content="https://turrero.vercel.app/promo.png"></meta>
-
-    </Head>
+  return (
     <div>
-      <div className={styles.wrapper}>
-        <Header totalTweets={Tweets.length} />
-        <div className={styles.content}>
-          <div className="graph-container" style={{ width: '100vw', height: '100vh' }}>
-            <svg ref={ref} style={{ width: '1vw', height: '1vh' }} />
-            <div id="tooltip" style={{position: 'absolute', visibility: 'hidden', backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}></div>
+      <Head>
+        <title>{title}</title>
+        <meta content="text/html; charset=UTF-8" name="Content-Type" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={summary} key="desc" />
+        <meta property='og:url' content='https://turrero.vercel.app/about'/>
+        <meta property="og:title" content="Sobre el proyecto de El Turrero Post - Las turras de Javier G. Recuenco" />
+        <meta property="og:description" content={summary} />
+        <meta property="og:image" content="https://turrero.vercel.app/promo.png"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@recuenco"/>
+        <meta name="twitter:creator" content="@k4rliky"/>
+        <meta name="twitter:title" content="Sobre el proyecto de El Turrero Post - Las turras de Javier G. Recuenco"/>
+        <meta name="twitter:description" content={summary}/>
+        <meta name="twitter:image" content="https://turrero.vercel.app/promo.png"></meta>
+      </Head>
+      <div>
+        <div className={styles.wrapper}>
+          <Header totalTweets={Tweets.length} />
+          <div className={styles.content}>
+            <div className="graph-container" style={{ width: '100vw', height: 'calc(100vh - 100px)', position: 'relative' }}>
+              <svg ref={ref} style={{ width: '100%', height: '100%' }} />
+              <div id="tooltip" style={{ position: 'absolute', visibility: 'hidden', backgroundColor: 'white', padding: '10px', borderRadius: '5px', boxShadow: '0px 0px 5px rgba(0,0,0,0.5)' }}></div>
+            </div>
           </div>
-          </div>
-        <Footer />
+          <Footer />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default GraphPage;
